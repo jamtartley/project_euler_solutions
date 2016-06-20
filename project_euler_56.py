b005e86e99cd459f9f640f2b0565cb569230ea90
Sam <samhartley92@gmail.com>
2016-06-20 18:22:37 +0100

    Problem 56
    A googol (10100) is a massive number: one followed by one-hundred zeros;
    100100 is almost unimaginably large: one followed by two-hundred zeros.
    Despite their size, the sum of the digits in each number is only 1.
    
    Considering natural numbers of the form, ab, where a, b < 100, what is
    the maximum digital sum?

---
+++ project_euler_56.py
@@ -0,0 +1,31 @@
+"""
+Project Euler 56
+
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+"""
+from time import time
+def getDigitalSum(number):
+    """
+    Gets the sum total of all digits of a given number
+    """
+    total = 0
+    for digit in str(number):
+        total += int(digit)
+    return total
+
+def main():
+    MAX_VALUE = 100
+    highest = -1
+
+    for a in range(1, MAX_VALUE):
+        for b in range(1, MAX_VALUE):
+            highest = max(highest, getDigitalSum(a**b))
+
+    print(highest)
+
+start = time()
+main()
+print(time() - start)
+    
